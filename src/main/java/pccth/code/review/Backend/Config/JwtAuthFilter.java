6e8375045607d4b8435f8d83e6fd70ee743cd79f
PCCKLA\tonkl <tonkla785@gmail.com>
2026-05-21 13:51:13 +0700

    fix docker n8n service

--- src/main/java/pccth/code/review/Backend/Config/JwtAuthFilter.java
+++ src/main/java/pccth/code/review/Backend/Config/JwtAuthFilter.java
@@ -4,7 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,9 +18,10 @@ import java.io.IOException;
 import java.util.List;
 
 @Component
-@Slf4j
+
 public class JwtAuthFilter extends OncePerRequestFilter {
 
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthFilter.class);
     private final JwtService jwtService;
 
     public JwtAuthFilter(JwtService jwtService) {
